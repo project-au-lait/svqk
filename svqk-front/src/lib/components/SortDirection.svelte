c83a0f4c88eb3f3fa91c948800143d31424a34db
Yuma-alt <pjp68792@gmail.com>
2024-09-19 17:25:23 +0900

    add component

---
+++ svqk-front/src/lib/components/SortDirection.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import SortOrderUtils from '$lib/arch/search/SortOrderUtils';
+
+  export let sortOrders;
+  export let label;
+  export let sortKey: string;
+
+  const dispatch = createEventDispatcher();
+
+  $: sortMark = SortOrderUtils.getSortMark(sortOrders, sortKey);
+
+  function onClick() {
+    dispatch('sort', { sortKey });
+  }
+</script>
+
+<th on:click={onClick}>
+  <span>{label}{sortMark}</span>
+</th>
