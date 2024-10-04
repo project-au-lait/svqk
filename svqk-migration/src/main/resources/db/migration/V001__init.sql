489eaae54f012326c0688045bb5c1c20b1e6e1bc
Yuma-alt <pjp68792@gmail.com>
2024-10-04 15:20:23 +0900

    add data

--- svqk-migration/src/main/resources/db/migration/V001__init.sql
+++ svqk-migration/src/main/resources/db/migration/V001__init.sql
@@ -12,11 +12,22 @@ VALUES('1', 'New'),
   ('3', 'Closed');
 
 
+CREATE TABLE tracker (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(128) NOT NULL,
+  --${commonColumns}
+);
+
+INSERT INTO tracker (name)
+VALUES ('バグ'), ('機能'), ('サポート');
+
+
 CREATE TABLE issue (
   id SERIAL PRIMARY KEY,
   subject VARCHAR(128) NOT NULL,
   due_date DATE,
   status_id CHAR(1) NOT NULL REFERENCES issue_status,
+  tracker_id INTEGER REFERENCES tracker,
   description VARCHAR(8192),
   --${commonColumns}
 )
