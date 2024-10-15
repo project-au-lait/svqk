796ba7c71b7dfcbaf0d8e1ffc646395f0fdba3f9
motoki.himeno <motoki.himeno@monocrea.co.jp>
2024-10-15 15:21:01 +0900

    refactor issue form

---
+++ svqk-front/src/lib/issue/IssueForm.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+  import { goto, invalidateAll } from '$app/navigation';
+  import type { IdModel, IssueModel, IssueStatusModel, TrackerModel } from '$lib/arch/api/Api';
+  import ApiHandler from '$lib/arch/api/ApiHandler';
+  import FormValidator from '$lib/arch/form/FormValidator';
+  import InputField from '$lib/arch/form/InputField.svelte';
+  import SelectBox from '$lib/arch/form/SelectBox.svelte';
+  import { messageStore } from '$lib/arch/global/MessageStore';
+  import { t } from '$lib/translations';
+  import { onMount } from 'svelte';
+  import * as yup from 'yup';
+
+  export let issue: IssueModel;
+  export let isNew: boolean;
+
+  let issueStatuses = [] as IssueStatusModel[];
+  let trackers = [] as TrackerModel[];
+
+  $: action = isNew ? $t('msg.register') : $t('msg.update');
+
+  const spec = {
+    subject: yup.string().required().label($t('msg.label.issue.subject'))
+  };
+
+  const form = FormValidator.createForm(spec, save);
+
+  onMount(async () => {
+    issueStatuses = (await ApiHandler.handle<IssueStatusModel[]>(fetch, (api) =>
+      api.issueStatuses.issueStatusesList()
+    ))!;
+
+    trackers = (await ApiHandler.handle<TrackerModel[]>(fetch, (api) =>
+      api.tracker.trackerList()
+    ))!;
+  });
+
+  async function save() {
+    const response = await ApiHandler.handle<IdModel>(fetch, (api) =>
+      api.issues.issuesCreate(issue)
+    );
+
+    if (response) {
+      if (isNew) {
+        await goto(`/issues/${response.id}`);
+      } else {
+        await invalidateAll();
+      }
+      messageStore.show($t('msg.saved'));
+    }
+  }
+</script>
+
+<form use:form>
+  <div>
+    <InputField id="subject" label={$t('msg.subject')} bind:value={issue.subject} />
+  </div>
+  <div>
+    <label for="description">{$t('msg.description')}</label>
+    <textarea id="description" style="width:100%" bind:value={issue.description}></textarea>
+  </div>
+  <div class="grid">
+    <div>
+      <SelectBox
+        id="status"
+        label={$t('msg.status')}
+        options={issueStatuses}
+        bind:value={issue.issueStatus.id}
+      />
+    </div>
+    <div>
+      <SelectBox
+        id="tracker"
+        label={$t('msg.tracker')}
+        options={trackers}
+        bind:value={issue.tracker.id}
+      />
+    </div>
+    <div>
+      <InputField id="dueDate" type="date" label={$t('msg.dueDate')} bind:value={issue.dueDate} />
+    </div>
+  </div>
+  <div>
+    <input id="save" type="submit" name="action" value={action} />
+  </div>
+</form>
---
+++ svqk-front/src/routes/issues/new/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { IssueModel, IssueStatusModel, TrackerModel } from '$lib/arch/api/Api';
+  import { pageStore } from '$lib/arch/global/PageStore';
+  import IssueForm from '$lib/issue/IssueForm.svelte';
+  import { t } from '$lib/translations';
+
+  const issue = { issueStatus: {} as IssueStatusModel, tracker: {} as TrackerModel } as IssueModel;
+
+  pageStore.setTitle($t('msg.newIssue'));
+</script>
+
+<IssueForm {issue} isNew={true} />
