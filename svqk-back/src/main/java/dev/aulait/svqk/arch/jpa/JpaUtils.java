b5f8bd90d3535c0b2d059412a4e061235002b4b8
motoki.himeno <motoki.himeno@monocrea.co.jp>
2024-12-10 09:24:23 +0900

    refactor:  fix fb

--- svqk-back/src/main/java/dev/aulait/svqk/arch/jpa/JpaUtils.java
+++ svqk-back/src/main/java/dev/aulait/svqk/arch/jpa/JpaUtils.java
@@ -2,43 +2,55 @@ package dev.aulait.svqk.arch.jpa;
 
 import static org.hibernate.jpa.SpecHints.HINT_SPEC_FETCH_GRAPH;
 
+import dev.aulait.svqk.arch.exception.ResourceNotFoundException;
 import jakarta.persistence.EntityGraph;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.JoinColumn;
 import java.lang.reflect.Field;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Stream;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class JpaUtils {
 
-  public static <T> EntityGraph<T> buildEntityGraph(
-      EntityManager em, Class<T> entityClass, List<String> attributeNodes) {
+  public static <T, I> T findByIdAsResource(JpaRepository<T, I> repository, I id) {
+    return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+  }
 
-    EntityGraph<T> entityGraph = em.createEntityGraph(entityClass);
-    entityGraph.addAttributeNodes(attributeNodes.toArray(String[]::new));
+  public static <T> T findWithFetch(
+      EntityManager em, Class<T> entityType, Object entityId, String... fetchFields) {
+
+    if (ArrayUtils.isEmpty(fetchFields)) {
+      fetchFields = getAllJoinColumnFields(entityType);
+    }
+
+    return Optional.ofNullable(
+            em.find(
+                entityType,
+                entityId,
+                Map.of(HINT_SPEC_FETCH_GRAPH, buildEntityGraph(em, entityType, fetchFields))))
+        .orElseThrow(() -> new ResourceNotFoundException(entityId));
+  }
+
+  public static <T> EntityGraph<T> buildEntityGraph(
+      EntityManager em, Class<T> entityType, String[] attributeNodes) {
+
+    EntityGraph<T> entityGraph = em.createEntityGraph(entityType);
+    entityGraph.addAttributeNodes(attributeNodes);
 
     return entityGraph;
   }
 
-  public static <T> Map<String, Object> buildFetchGraph(EntityManager em, Class<T> entityClass) {
-    return buildFetchGraph(em, entityClass, getAllJoinColumnFields(entityClass));
-  }
-
-  public static <T> Map<String, Object> buildFetchGraph(
-      EntityManager em, Class<T> entityClass, List<String> fetchFields) {
-
-    return Map.of(HINT_SPEC_FETCH_GRAPH, buildEntityGraph(em, entityClass, fetchFields));
-  }
-
-  private static <T> List<String> getAllJoinColumnFields(Class<T> entityClass) {
-    return Stream.of(entityClass.getDeclaredFields())
+  private static <T> String[] getAllJoinColumnFields(Class<T> entityType) {
+    return Stream.of(entityType.getDeclaredFields())
         .filter(f -> Objects.nonNull(f.getAnnotation(JoinColumn.class)))
         .map(Field::getName)
-        .toList();
+        .toArray(String[]::new);
   }
 }
--- svqk-back/src/main/java/dev/aulait/svqk/domain/issue/IssueRepository.java
+++ svqk-back/src/main/java/dev/aulait/svqk/domain/issue/IssueRepository.java
@@ -1,20 +1,16 @@
 package dev.aulait.svqk.domain.issue;
 
-import static dev.aulait.svqk.arch.jpa.JpaUtils.buildFetchGraph;
+import static dev.aulait.svqk.arch.jpa.JpaUtils.findWithFetch;
 
-import dev.aulait.svqk.arch.exception.ResourceNotFoundException;
 import jakarta.persistence.EntityManager;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface IssueRepository extends JpaRepository<IssueEntity, Integer> {
 
   default IssueEntity findByIdWithDetails(Integer id, EntityManager em) {
-    return Optional.ofNullable(
-            em.find(IssueEntity.class, id, buildFetchGraph(em, IssueEntity.class)))
-        .orElseThrow(() -> new ResourceNotFoundException(id));
+    return findWithFetch(em, IssueEntity.class, id);
   }
 
   @Query(
