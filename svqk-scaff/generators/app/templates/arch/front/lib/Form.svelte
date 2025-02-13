47038c0e966b2283e35d3035aba0d668073f2c8f
TsuchiyaTatsuki <37523281+TsuchiyaTatsuki@users.noreply.github.com>
2025-02-13 13:39:04 +0900

    feat: add support for scaffolding entity registration page (#60)

---
+++ svqk-scaff/generators/app/templates/arch/front/lib/Form.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import type { <%= entityNmPascal %>Model } from '$lib/arch/api/Api';
+  import ApiHandler from '$lib/arch/api/ApiHandler';
+  import FormValidator from '$lib/arch/form/FormValidator';
+  import InputField from '$lib/arch/form/InputField.svelte';
+  import { messageStore } from '$lib/arch/global/MessageStore';
+  import { t } from '$lib/translations';
+  import { string } from 'yup';
+
+  interface Props {
+    <%= entityNmCamel %>: <%= entityNmPascal %>Model;
+    handleAfterSave: (id?: number) => Promise<void>;
+    actionBtnLabel: string;
+  }
+
+  let { <%= entityNmCamel %> = $bindable(), handleAfterSave, actionBtnLabel }: Props = $props();
+
+  const spec = {
+    <%_ for (field of fields) { _%>
+      <%_ if (field.required) { _%>
+        <%= field.fieldName %>: string().required().label($t('msg.label.<%= entityNmCamel %>.<%= field.fieldName %>')),
+      <%_ } _%>
+    <%_ } _%>
+  };
+
+  const form = FormValidator.createForm(spec, save);
+
+  async function save() {
+    const response = await ApiHandler.handle<number>(fetch, (api) => 
+      api.<%= entityNmCamel %>.<%= entityNmCamel %>Create(<%= entityNmCamel %>));
+
+    if (response) {
+      await handleAfterSave(response);
+      messageStore.show($t('msg.saved'));
+    }
+  }
+</script>
+
+<form use:form>
+  <%_ for (field of fields) { _%>
+    <div>
+      <InputField id="<%= field.fieldName %>" label={$t(`msg.label.<%= entityNmCamel %>.<%= field.fieldName %>`)} bind:value={<%= entityNmCamel %>.<%= field.fieldName %>} />
+    </div>
+  <%_ } _%>
+  <div>
+    <button id="save" type="submit">{actionBtnLabel}</button>
+  </div>
+</form>
---
+++ svqk-scaff/generators/app/templates/arch/front/routes/new/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { goto } from '$app/navigation';
+  import type { <%= entityNmPascal %>Model } from '$lib/arch/api/Api';
+  import <%= entityNmPascal %>Form from '$lib/domain/<%= entityNmPlural %>/<%= entityNmPascal %>Form.svelte';
+  import { t } from '$lib/translations';
+
+  let <%= entityNmCamel %> = $state({} as <%= entityNmPascal %>Model);
+
+  async function handleAfterSave(id?: number) {
+    await goto(`/<%= entityNmPlural %>/${id}`);
+  }
+</script>
+
+<<%= entityNmPascal %>Form bind:<%= entityNmCamel %> {handleAfterSave} actionBtnLabel={$t('msg.register')} />
