abd9efdb1ec18cd1ff7ea872cbd28284e80561ed
motoki.himeno <motoki.himeno@monocrea.co.jp>
2025-02-18 15:55:39 +0900

    fix: fix indent

--- svqk-scaff/generators/app/templates/arch/back/Dto.java
+++ svqk-scaff/generators/app/templates/arch/back/Dto.java
@@ -17,12 +17,12 @@ public class <%= entityNmPascal %>Dto {
 <%_ fields.forEach(field => { -%>
 
   <%_ if (field.required) { -%>
-    @Schema(required = true)
-    <%_ } -%>
-    <%_ if (compIdFields && field.id) { -%>
-    private <%= entityNmPascal %>IdDto <%= field.fieldName %>;
-    <%_ } else { -%>
-    private <%- field.multiple ? `List<${field.javaType}>` : field.javaType %> <%= field.fieldName %>;
-    <%_ } -%>
-  <%_ }); -%>
+  @Schema(required = true)
+  <%_ } -%>
+  <%_ if (compIdFields && field.id) { -%>
+  private <%= entityNmPascal %>IdDto <%= field.fieldName %>;
+  <%_ } else { -%>
+  private <%- field.multiple ? `List<${field.javaType}>` : field.javaType %> <%= field.fieldName %>;
+  <%_ } -%>
+<%_ }); -%>
 }
