cea9b2410c68cb26201c65f9736158022db075e1
TsuchiyaTatsuki <kakakikiup1@gmail.com>
2025-03-12 16:02:34 +0900

    Add sortOrders field to Dto

--- svqk-backend/src/main/java/dev/aulait/svqk/interfaces/issue/IssueFactory.java
+++ svqk-backend/src/main/java/dev/aulait/svqk/interfaces/issue/IssueFactory.java
@@ -35,7 +35,7 @@ public class IssueFactory {
         .where("i.issueStatus.id", IN, criteria.getIssueStatuses())
         .where("i.dueDate", criteria.getDueDate())
         .defaultOrderBy("i.id", false)
-        .build(criteria.getPageControl()); // <.>
+        .build(criteria.getPageControl(), criteria.getPageControl().getSortOrders()); // <.>
   }
 
   public IssueTrackingDto buildIssueTracking(List<IssueTrackingRs> issueTrackings) {
