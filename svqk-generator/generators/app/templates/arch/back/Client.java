502af9cb45586154386bc7bbc3e58fbc191bad63
tmatsuura11 <196127613+tmatsuura11@users.noreply.github.com>
2025-03-17 15:08:18 +0900

    support: add backend and it of delete function to generator

--- svqk-generator/generators/app/templates/arch/back/Client.java
+++ svqk-generator/generators/app/templates/arch/back/Client.java
@@ -1,18 +1,25 @@
 <%_ include('../../lib/interface-common', { idField, compIdFields }); -%>
 <%_
-getMethodArgs = buildArgs((field) => `${field.javaType} ${field.fieldName}`);
+idMethodArgs = buildArgs((field) => `${field.javaType} ${field.fieldName}`);
 givenGetArgs = buildArgs((field) => field.fieldName);
 -%>
 package <%= interfacesPkgNm %>;
 
 import static <%= interfacesPkgNm %>.<%= entityNmPascal %>Controller.*;
 import static <%= interfacesPkgNm %>.<%= entityNmPascal %>Controller.<%= entityNmPascal %>SearchResultDto;
+import dev.aulait.svqk.arch.exception.ErrorResponseDto;
 import dev.aulait.svqk.arch.test.RestClientUtils;
 
 public class <%= entityNmPascal %>Client {
 
-  public <%= entityNmPascal %>Dto get(<%= getMethodArgs %>) {
-    return RestClientUtils.get(<%= entityNmAllCaps %>_PATH + "/" + <%= entityNmAllCaps %>_GET_PATH, <%= entityNmPascal %>Dto.class, <%= givenGetArgs %>);
+  private static final String <%= entityNmAllCaps %>_AND_<%= entityNmAllCaps %>_ID_PATH = <%= entityNmAllCaps %>_PATH + "/" + <%= entityNmAllCaps %>_ID_PATH;
+
+  public <%= entityNmPascal %>Dto get(<%= idMethodArgs %>) {
+    return RestClientUtils.get(<%= entityNmAllCaps %>_AND_<%= entityNmAllCaps %>_ID_PATH, <%= entityNmPascal %>Dto.class, <%= givenGetArgs %>);
+  }
+
+  public ErrorResponseDto getWithError(<%= idMethodArgs %>) {
+    return RestClientUtils.getWithError(<%= entityNmAllCaps %>_AND_<%= entityNmAllCaps %>_ID_PATH, <%= givenGetArgs %>);
   }
 
   public <%= interfaceIdType %> save(<%= entityNmPascal %>Dto dto) {
@@ -21,6 +28,10 @@ public class <%= entityNmPascal %>Client {
 
   public <%= interfaceIdType %> update(<%= entityNmPascal %>Dto dto) {
     return RestClientUtils.put(<%= entityNmAllCaps %>_PATH, dto, <%= interfaceIdType %>.class);
+  }
+
+  public <%= interfaceIdType %> delete(<%= idMethodArgs %>, <%= entityNmPascal %>Dto <%= entityNmCamel %>) {
+    return RestClientUtils.delete(<%= entityNmAllCaps %>_AND_<%= entityNmAllCaps %>_ID_PATH, <%= entityNmCamel %>, <%= interfaceIdType %>.class, <%= givenGetArgs %>);
   }
 
   public <%= entityNmPascal %>SearchResultDto search(<%= entityNmPascal %>SearchCriteriaDto dto) {
--- svqk-generator/generators/app/templates/arch/back/Service.java
+++ svqk-generator/generators/app/templates/arch/back/Service.java
@@ -3,6 +3,7 @@ package <%= domainPkgNm %>;
 import dev.aulait.svqk.arch.jpa.SearchUtils;
 import dev.aulait.svqk.arch.search.SearchCriteriaVo;
 import dev.aulait.svqk.arch.search.SearchResultVo;
+import dev.aulait.svqk.arch.exception.ResourceNotFoundException;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
@@ -17,12 +18,18 @@ public class <%= entityNmPascal %>Service {
   private final <%= entityNmPascal %>Repository <%= entityNmCamel %>Repository;
 
   public <%= entityNmPascal %>Entity find(<%= idField.javaType %> id) {
-    return <%= entityNmCamel %>Repository.findById(id).orElseThrow(IllegalArgumentException::new);
+    return <%= entityNmCamel %>Repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(1));
   }
 
   @Transactional
   public <%= entityNmPascal %>Entity save(<%= entityNmPascal %>Entity entity) {
     return <%= entityNmCamel %>Repository.save(entity);
+  }
+
+  @Transactional
+  public void delete(<%= entityNmPascal %>Entity entity) {
+    <%= entityNmPascal %>Entity managedEntity = em.merge(entity);
+    <%= entityNmCamel %>Repository.delete(managedEntity);
   }
 
   public SearchResultVo<<%= entityNmPascal %>Entity> search(SearchCriteriaVo criteria) {
