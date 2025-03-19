3771b94e796f56f120b9f913007227c50acbae62
TsuchiyaTatsuki <kakakikiup1@gmail.com>
2025-03-19 14:54:53 +0900

    Undo unwanted modifications

--- svqk-generator/generators/app/templates/arch/front/routes/entityId/+page.ts
+++ svqk-generator/generators/app/templates/arch/front/routes/entityId/+page.ts
@@ -1,5 +1,7 @@
 <%_
-apiCallArgs = ifcom.idFields.map((idField) => idField.javaType == "Integer" ? `Number(params.${idField.fieldName})` : `params.${idField.fieldName}`).join(", ");
+idFields = compIdFields ?? [idField];
+
+apiCallArgs = idFields.map((idField) => idField.javaType == "Integer" ? `Number(params.${idField.fieldName})` : `params.${idField.fieldName}`).join(", ");
 %>
 import type { <%= entityNmPascal %>Model } from '$lib/arch/api/Api';
 import ApiHandler from '$lib/arch/api/ApiHandler';
