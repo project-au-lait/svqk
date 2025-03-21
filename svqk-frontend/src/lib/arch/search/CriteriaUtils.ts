248c9a89677b21a8c3bbf16fde91d59e8ed6e2a3
Yuichi Kuwahara <yuichi.kuwahara.0@gmail.com>
2025-03-21 15:21:53 +0900

    refactor: add a utility for handling search criteria in the frontend

---
+++ svqk-frontend/src/lib/arch/search/CriteriaUtils.ts
@@ -0,0 +1,44 @@
+import rison from 'rison';
+
+export default class CriteriaUtils {
+  private static readonly PARAM_KEY_CRITERIA = 'c';
+  private static readonly PARAM_KEY_OPTION = 'o';
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static encode(criteria: any, option?: any): string {
+    const criteriaString = `?${this.PARAM_KEY_CRITERIA}=${this.encodeObj(criteria)}`;
+
+    if (!option) {
+      return criteriaString;
+    }
+
+    return criteriaString + `&${this.PARAM_KEY_OPTION}=${this.encodeObj(option)}`;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static decode(url: URL): any {
+    const criteria = this.decodeObj(url, this.PARAM_KEY_CRITERIA);
+
+    if (!criteria.pageControl) {
+      criteria.pageControl = {
+        pageNumber: 1
+      };
+    }
+
+    return criteria;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static decodeOption(url: URL): any {
+    return this.decodeObj(url, this.PARAM_KEY_OPTION);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private static encodeObj(obj: any) {
+    return rison.encode(JSON.stringify(obj));
+  }
+
+  private static decodeObj(url: URL, key: string) {
+    return JSON.parse(rison.decode(url.searchParams.get(key) ?? '{}'));
+  }
+}
