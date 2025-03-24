4642b8b9ca3cfdffa4e33edd80f39d7e42f0c5b4
j-shimizu1201 <junya.shimizu@monocrea.co.jp>
2025-03-24 14:38:56 +0900

    refactor: generator decode encode

--- svqk-generator/generators/app/templates/arch/front/routes/list/+page.ts
+++ svqk-generator/generators/app/templates/arch/front/routes/list/+page.ts
@@ -3,20 +3,20 @@ import ApiHandler from '$lib/arch/api/ApiHandler';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch, url }) => {
-  const condition = {
-    pageControl: {
-      pageNumber: 1
-    },
-    ...JSON.parse(decodeURIComponent(url.searchParams.get('q') ?? '{}'))
+  const criteria = {
+    ...CriteriaUtils.decode(url)
   } as <%= entityNmPascal %>SearchCriteriaModel;
+
+  const open = CriteriaUtils.decodeParam<boolean>(url, 'open') ?? false;
 
   const result =
     (await ApiHandler.handle<<%= entityNmPascal %>SearchResultModel>(fetch, (api) =>
-      api.<%= entityNmCamel %>.<%= entityNmCamel %>Search(condition)
+      api.<%= entityNmCamel %>.<%= entityNmCamel %>Search(criteria)
     )) || ({} as <%= entityNmPascal %>SearchResultModel);
 
   return {
-    condition,
+    criteria,
+    open,
     result
   };
 };
