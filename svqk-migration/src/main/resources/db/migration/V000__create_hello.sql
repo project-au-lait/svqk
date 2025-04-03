e6c1b0fd26ed513982b173864700db5393a367df
tmatsuura11 <196127613+tmatsuura11@users.noreply.github.com>
2025-04-03 14:11:09 +0900

    chore: change sql related to hello_world

--- svqk-migration/src/main/resources/db/migration/V000__create_hello.sql
+++ svqk-migration/src/main/resources/db/migration/V000__create_hello.sql
@@ -19,13 +19,55 @@ VALUES (1, 'Hello');
 
 
 CREATE table hello_world (
-    id INT,
-    seq_no INT,
-    message VARCHAR(255) NOT NULL,
+    int_id INT,
+    char_id VARCHAR(128),
+    bool_id BOOLEAN,
+    date_id DATE,
+    timestamp_id TIMESTAMP,
+    text_id VARCHAR(129),
+    int_col INT,
+    char_col VARCHAR(128),
+    bool_col BOOLEAN,
+    date_col DATE,
+    timestamp_col TIMESTAMP,
+    text_col VARCHAR(129),
     --${commonColumns},
-    PRIMARY KEY (id, seq_no)
+    PRIMARY KEY (
+        int_id,
+        char_id,
+        bool_id,
+        date_id,
+        timestamp_id,
+        text_id
+    )
 );
 
 
-INSERT INTO hello_world (id, seq_no, message)
-VALUES (1, 1, 'Hello World');
+INSERT INTO hello_world (
+        int_id,
+        char_id,
+        bool_id,
+        date_id,
+        timestamp_id,
+        text_id,
+        int_col,
+        char_col,
+        bool_col,
+        date_col,
+        timestamp_col,
+        text_col
+    )
+VALUES (
+        1,
+        '1',
+        TRUE,
+        '2025-03-31',
+        '2025-03-31 00:00:00',
+        'Hello World',
+        1,
+        '1',
+        TRUE,
+        '2025-03-31',
+        '2025-03-31 00:00:00',
+        'Hello World'
+    );
