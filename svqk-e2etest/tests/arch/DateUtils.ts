ab3a9dc50e3af9aa30df3016c50aa9ce93002b2d
tmatsuura11 <196127613+tmatsuura11@users.noreply.github.com>
2025-04-03 14:35:29 +0900

    refactor: refactor DateUtils.ts of e2e

--- svqk-e2etest/tests/arch/DateUtils.ts
+++ svqk-e2etest/tests/arch/DateUtils.ts
@@ -5,22 +5,11 @@ export default class DateUtils {
     return new Date(Math.floor(Math.random() * new Date().getTime()));
   }
 
-  static generateRandomLocalDateTime(): LocalDate {
-    const date = new Date(
-      Date.now() - Math.floor(Math.random() * 10 * 365 * 24 * 60 * 60 * 1000) // 約10年分の範囲でランダム
-    );
-
-    const pad = (n: number) => n.toString().padStart(2, '0');
-    const yyyy = date.getFullYear();
-    const mm = pad(date.getMonth() + 1);
-    const dd = pad(date.getDate());
-    const hh = pad(date.getHours());
-    const min = pad(date.getMinutes());
-
-    return `${yyyy}-${mm}-${dd}T${hh}:${min}`;
+  static generateRandomLocalDateTime(): LocalDateTime {
+    return DateUtils.generateRandomDate().toISOString().slice(0, 16);
   }
 
-  static generateRandomLocalDate(): LocalDateTime {
+  static generateRandomLocalDate(): LocalDate {
     return DateUtils.generateRandomDate().toISOString().split('T')[0];
   }
 }
