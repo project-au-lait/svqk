1d31934aba567e2f55e82d10f06efb5690071e47
tmatsuura11 <196127613+tmatsuura11@users.noreply.github.com>
2025-04-04 11:38:51 +0900

    refactor: add buildDetailApiCall and use it

--- svqk-generator/generators/app/templates/arch/frontend/routes/entityId/+page.ts
+++ svqk-generator/generators/app/templates/arch/frontend/routes/entityId/+page.ts
@@ -1,25 +1,11 @@
-<%_
-idFields = compIdFields ?? [idField];
-
-const convertParam = (field) => {
-  if (field.javaType === "Integer") {
-    return `Number(params.${field.fieldName})`;
-  } else if (field.javaType === "Boolean") {
-    return `params.${field.fieldName} === 'true'`;
-  } else {
-    return `params.${field.fieldName}`;
-  }
-};
-
-apiCallArgs = idFields.map(convertParam).join(", ");
-%>
+<%_ include('../../../../lib/typescript-common'); -%>
 import type { <%= entityNmPascal %>Model } from '$lib/arch/api/Api';
 import ApiHandler from '$lib/arch/api/ApiHandler';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch, params }) => {
   const <%= entityNmCamel %> = (await ApiHandler.handle<<%= entityNmPascal %>Model>(fetch, (api) =>
-    api.<%= entityNmCamel %>.<%= entityNmCamel %>Detail(<%- apiCallArgs %>)
+    <%- tscom.buildDetailApiCall(entityNmCamel, compIdFields) %>
   ))!;
 
   return {
