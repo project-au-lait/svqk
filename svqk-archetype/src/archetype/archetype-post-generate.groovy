3af8cbe78e7f8ecad0dd43578a2971a218e8d994
tmatsuura11 <196127613+tmatsuura11@users.noreply.github.com>
2025-06-13 16:35:43 +0900

    feat: exec chmod u+x mvnw for mac in groovy post generate script

--- svqk-archetype/src/archetype/archetype-post-generate.groovy
+++ svqk-archetype/src/archetype/archetype-post-generate.groovy
@@ -1,3 +1,18 @@
+def renameFile(String fromPath, String toPath) {
+    def file = new File(fromPath)
+    file.renameTo(new File(toPath))
+}
 
-def file = new File(request.artifactId + '/.gitignore.archetype')
-file.renameTo request.artifactId + '/.gitignore'
+def makeFileExecutable(String path) {
+    def file = new File(path)
+    file.setExecutable(true, true)
+}
+
+def projectDir = request.artifactId
+
+renameFile("${projectDir}/.gitignore.archetype", "${projectDir}/.gitignore")
+
+def osName = System.getProperty("os.name").toLowerCase()
+if (!osName.contains("windows")) {
+    makeFileExecutable("${projectDir}/mvnw")
+}
