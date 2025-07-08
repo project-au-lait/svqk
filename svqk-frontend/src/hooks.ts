16b41994e3980d71a8f53f390118ece038b62fab
tmatsuura11 <196127613+tmatsuura11@users.noreply.github.com>
2025-07-08 13:57:00 +0900

    chore: add paraglide-js

--- svqk-frontend/src/hooks.ts
+++ svqk-frontend/src/hooks.ts
@@ -1,3 +1,6 @@
+import { deLocalizeUrl } from '$lib/paraglide/runtime';
 // Modules to be registered in the DI container
 import '$lib/domain/issue/IssueStatusMasterStore'; // <.>
 import '$lib/domain/issue/TrackerMasterStore';
+
+export const reroute = (request) => deLocalizeUrl(request.url).pathname;
--- svqk-frontend/vite.config.ts
+++ svqk-frontend/vite.config.ts
@@ -1,6 +1,13 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    paraglideVitePlugin({
+      project: './project.inlang',
+      outdir: './src/lib/paraglide'
+    })
+  ]
 });
