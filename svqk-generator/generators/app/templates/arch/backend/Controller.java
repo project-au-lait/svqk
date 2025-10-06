e7648ba592e1665df97c770572ba2ae20db2d0f3
kenji.kimura <kenji.kimura@monocrea.co.jp>
2025-10-06 13:27:15 +0900

    Reapply "fix: remove unnecessary annotations from the generator (#140)"
    This reverts commit 9907efd12d52a090bf5429bd4554eb321e239fff.

--- svqk-generator/generators/app/templates/arch/backend/Controller.java
+++ svqk-generator/generators/app/templates/arch/backend/Controller.java
@@ -2,10 +2,6 @@
 <%_
 idPath = ifcom.idFields.map((field) => `{${field.fieldName}}`).join("/");
 idMethodArgs = ifcom.buildArgs((field) => `@PathParam("${field.fieldName}") ${field.javaType} ${field.fieldName}`);
-parametersAnnotation = `@Parameters({\n  ` +
-  ifcom.idFields.map((field) =>
-    `@Parameter(name = "${field.fieldName}", in = ParameterIn.PATH, required = true)`
-  ).join(",\n  ") + `\n})`;
 -%>
 package <%= interfacesPkgNm %>;
 
@@ -21,9 +17,6 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import lombok.RequiredArgsConstructor;
-import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
-import org.eclipse.microprofile.openapi.annotations.parameters.Parameters;
-import org.eclipse.microprofile.openapi.annotations.enums.ParameterIn;
 
 @Path(<%= entityNmPascal %>Controller.<%= entityNmAllCaps %>_PATH)
 @RequiredArgsConstructor
@@ -40,7 +33,6 @@ public class <%= entityNmPascal %>Controller {
 
   @GET
   @Path(<%= entityNmAllCaps %>_ID_PATH)
-  <%- parametersAnnotation %>
   public <%= entityNmPascal %>Dto get(<%- idMethodArgs %>) {
 <%= ifcom.buildEntity %>
 
@@ -58,7 +50,6 @@ public class <%= entityNmPascal %>Controller {
 
   @PUT
   @Path(<%= entityNmAllCaps %>_ID_PATH)
-  <%- parametersAnnotation %>
   public <%= ifcom.interfaceIdType %> update(<%- idMethodArgs %>, @Valid <%= entityNmPascal %>Dto dto) {
     <%= entityNmPascal %>Entity entity = BeanUtils.map(dto, <%= entityNmPascal %>Entity.class);
 
@@ -71,7 +62,6 @@ public class <%= entityNmPascal %>Controller {
 
   @DELETE
   @Path(<%= entityNmAllCaps %>_ID_PATH)
-  <%- parametersAnnotation %>
   public <%= ifcom.interfaceIdType %> delete(<%- idMethodArgs %>, @Valid <%= entityNmPascal %>Dto dto) {
     <%= entityNmPascal %>Entity entity = BeanUtils.map(dto, <%= entityNmPascal %>Entity.class);
 
